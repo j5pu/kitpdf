7d0c37103137d862607acdc873f2d8945cc28caf
root <root@example.com>
2023-10-14 22:32:28 +0200

    fix:

--- src/kitpdf/__init__.py
+++ src/kitpdf/__init__.py
@@ -11,8 +11,11 @@ __all__ = (
     "pdf_reduce",
     "pdf_scan",
     "pdf_to_picture",
+    "putalpha_random",
+    "white_alpha",
 )
 
+import contextlib
 import difflib
 import random
 import shutil
@@ -22,7 +25,10 @@ from typing import Literal
 
 import fitz
 import nodeps
-from nodeps import Path
+import numpy as np
+from nodeps import AnyPath, Path
+from PIL import Image
+from PIL.Image import Resampling
 
 PDFBOX_DATA = Path(__file__).parent / "data"
 PDFBOX_DATA_TESTS = PDFBOX_DATA / "tests"
@@ -196,17 +202,258 @@ def pdf_scan(file: Path, directory: Path | None = None) -> Path:
     return dest
 
 
-def pdf_to_picture(file: Path | str, dpi: int = 300, fmt: Literal["jpeg", "png"] = "jpeg") -> Path:
-    """Creates a file with jpeg in the same directory from first page of pdf."""
+@contextlib.contextmanager
+def pdf_to_picture(source: AnyPath, dest: AnyPath | Literal["dir", "tmp"] = "dir", dpi: int = 300,
+                   fmt: Literal["jpeg", "png"] = "png") -> Path:
+    """Creates a file with jpeg in the same directory from first page of pdf.
+
+    Examples:
+        >>> from kitpdf import PDFBOX_DATA_TESTS
+        >>> from kitpdf import pdf_to_picture
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "BBVA.pdf"
+        >>>
+        >>> with pdf_to_picture(src, PDFBOX_DATA_TESTS / f"generated/BBVA-{putalpha_random.__name__}.png") as output:
+        ...     assert output.exists()
+        ...     assert output.suffix == ".png"
+        >>>
+        >>> with pdf_to_picture(src, "tmp") as temp:
+        ...     assert temp.exists()
+        ...     assert temp.suffix == ".png"
+        >>>
+        >>> with pdf_to_picture(src) as png:
+        ...     assert png.exists()
+        ...     assert png.suffix == ".png"
+
+    Arguments:
+        source: Source pdf to converto to picture
+        dest: Destination path, dir to use the same same path with different suffix or tmp for temp file
+        dpi: dpi
+        fmt: output jpeg or png
+
+    Returns:
+        Temp path with new image or destination
+    """
     nodeps.which("pdftoppm")
 
-    file = Path(file)
+    source = Path(source)
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        tmp = Path(tmpdir) / "tmp"
-        subprocess.run(["pdftoppm", f"-{fmt}", "-r", str(dpi), "-singlefile", file, tmp])
-        suffix = f".{fmt}" if fmt == "png" else ".jpg"
-        if not (dest := tmp.with_suffix(suffix)).exists():
-            msg = f"File not found {dest}"
-            raise FileNotFoundError(msg)
-        return shutil.copy(dest, file.with_suffix(suffix))
+    try:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmp = Path(tmpdir) / "tmp"
+            subprocess.run(["pdftoppm", f"-{fmt}", "-r", str(dpi), "-singlefile", source, tmp])
+            suffix = f".{fmt}" if fmt == "png" else ".jpg"
+            if not (tmp := tmp.with_suffix(suffix)).exists():
+                msg = f"File not found {tmp}"
+                raise FileNotFoundError(msg)
+            if dest == "dir":
+                yield shutil.copy(tmp, source.with_suffix(suffix))
+            elif dest == "tmp":
+                yield tmp
+            else:
+                yield shutil.copy(tmp, dest)
+    finally:
+        pass
+
+
+@contextlib.contextmanager
+def picture_paste(
+        background: AnyPath,
+        foreground: AnyPath,
+        dest: AnyPath = None,
+        putalpha: bool = True,
+        position: tuple[int, int] | tuple[int, int, int, int] | None = (0, 0),
+        stamp: bool = False,
+):
+    """Paste the foreground image on top of the background image.
+
+    Examples:
+        >>> from kitpdf import PDFBOX_DATA_TESTS
+        >>> from kitpdf import picture_paste
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "BBVA.png"
+        >>> fo = PDFBOX_DATA_TESTS / "folded.png"
+        >>>
+        >>> with picture_paste(fo, src, PDFBOX_DATA_TESTS / f"generated/folded-BBVA-{picture_paste.__name__}.png") as o:
+        ...     assert o.exists()
+        ...     assert o.suffix == ".png"
+        >>>
+        >>> with picture_paste(fo, src) as temp:
+        ...     assert temp.exists()
+        ...     assert temp.suffix == ".png"
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "BioSalud Stamp Transparent.png"
+        >>> d = PDFBOX_DATA_TESTS / f"generated/BioSalud Stamp Transparent-{picture_paste.__name__}.png"
+        >>> with picture_paste(fo, src, d, position=(300, 420)) as o:
+        ...     assert o.exists()
+        ...     assert o.suffix == ".png"
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "generated/BBVA-white_alpha.png"
+        >>> d = PDFBOX_DATA_TESTS / f"generated/BBVA-white_alpha-{picture_paste.__name__}.png"
+        >>> with picture_paste(fo, src, d, position=(0, 210)) as o:
+        ...     assert o.exists()
+        ...     assert o.suffix == ".png"
+        >>>
+
+    Arguments:
+        background: Background image
+        foreground: Foreground image
+        dest: None for temp path or dest
+        putalpha: Put alpha channel (transparency) to random value `putalpha_random()` before pasting
+        position: position of foreground image, if position is (0,0) background is resized to same size as forground
+        stamp: True to stamp the foreground image
+    """
+    if foreground is None:
+        return background
+
+    bg = Image.open(background).convert("RGBA")
+    fg = Image.open(foreground).convert("RGBA")
+
+    if stamp:
+        putalpha = False
+        x = random.randint(int(0.1 * bg.size[0]), int(0.7 * bg.size[0]))  # noqa: S311
+        y = random.randint(int(0.6 * bg.size[1]), int(0.85 * bg.size[1]))  # noqa: S311
+        puntos = Image.open(PDFBOX_DATA / "Puntos.png").convert("RGBA")
+        puntos = puntos.resize(fg.size, resample=Resampling.NEAREST)
+        puntos = puntos.rotate(random.randint(0, 180))  # noqa: S311
+        fg.paste(puntos, (0, 0), puntos)
+        fg = fg.resize(fg.size, resample=Resampling.NEAREST)
+        fg = fg.rotate(random.randint(8, 48), expand=True)  # noqa: S311
+        position = (x, y)
+
+    if position == (0, 0):
+        bg = bg.resize(fg.size)
+
+    try:
+        if putalpha:
+            with putalpha_random(foreground) as fg:
+                f = Image.open(fg).convert("RGBA")
+                bg.paste(f, position, f)
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    tmp = Path(tmpdir) / "paste.png"
+                    bg.save(tmp, "PNG")
+                    if dest is None:
+                        yield tmp
+                    else:
+                        yield shutil.copy(tmp, dest)
+        else:
+            bg.paste(fg, position, fg)
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmp = Path(tmpdir) / "paste.png"
+                bg.save(tmp, "PNG")
+                if dest is None:
+                    yield tmp
+                else:
+                    yield shutil.copy(tmp, dest)
+    finally:
+        pass
+
+
+@contextlib.contextmanager
+def putalpha_random(source: AnyPath, dest: AnyPath = None, value: tuple[int, int] = (0.62, 0.72)) -> Path:
+    """Put alpha channel (transparency) to random value.
+
+    Examples:
+        >>> from kitpdf import putalpha_random
+        >>> from kitpdf import PDFBOX_DATA_TESTS
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "BBVA.pdf"
+        >>> with putalpha_random(src, PDFBOX_DATA_TESTS / f"generated/BBVA-{putalpha_random.__name__}.png", ) as out:
+        ...     assert out.exists()
+        ...     assert out.suffix == ".png"
+        >>> with (putalpha_random(src) as temp):
+        ...     assert temp.exists()
+        ...     assert temp.suffix == ".png"
+
+    Arguments:
+        source: Source image, or pdf to converto to png
+        dest: Destination path or None for temp file
+        value: Tuple of min and max value
+
+    Returns:
+        Temp path with new image or destination
+    """
+    @contextlib.contextmanager
+    def _putalpha_random(src) -> Path:
+        img = Image.open(src)
+        img.putalpha(int(random.uniform(*value) * 255))  # noqa: S311
+        try:
+            if dest:
+                img.save(dest)
+                yield Path(dest)
+            else:
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    tmp = Path(tmpdir) / "tmp.png"
+                    img.save(tmp)
+                    if not tmp.exists():
+                        msg = f"File not found {tmp}"
+                        raise FileNotFoundError(msg)
+                    yield tmp
+        finally:
+            pass
+
+    source = Path(source)
+
+    if source.suffix == ".pdf":
+        with (pdf_to_picture(source) as png, _putalpha_random(png) as alpha):
+            yield alpha
+    else:
+        with _putalpha_random(source) as alpha:
+            yield alpha
+
+
+@contextlib.contextmanager
+def white_alpha(source: AnyPath, dest: AnyPath = None) -> Path:
+    """Make the white pixels transparent.
+
+    Examples:
+        >>> from kitpdf import white_alpha
+        >>> from kitpdf import PDFBOX_DATA_TESTS
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "Biosalud Stamp.png"
+        >>> with white_alpha(src, PDFBOX_DATA_TESTS / f"generated/Biosalud Stamp-{white_alpha.__name__}.png", ) as out:
+        ...     assert out.exists()
+        ...     assert out.suffix == ".png"
+        >>> with white_alpha(src) as temp:
+        ...     assert temp.exists()
+        ...     assert temp.suffix == ".png"
+        >>>
+        >>> src = PDFBOX_DATA_TESTS / "BBVA.png"
+        >>> with white_alpha(src, PDFBOX_DATA_TESTS / f"generated/BBVA-{white_alpha.__name__}.png", ) as out:
+        ...     assert out.exists()
+        ...     assert out.suffix == ".png"
+
+
+    Arguments:
+        source: Source image, or pdf to converto to png
+        dest: Destination path or None for temp file
+
+    Returns:
+        path temp or dest
+    """
+    image = np.asarray(Image.open(source).convert("RGBA"))
+    r, g, b, a = np.rollaxis(image, axis=-1)  # split into 4 n x m arrays
+    r_m = r != 255  # binary mask for red channel, True for all non white values  # noqa: PLR2004
+    g_m = g != 255  # binary mask for green channel, True for all non white values  # noqa: PLR2004
+    b_m = b != 255  # binary mask for blue channel, True for all non white values  # noqa: PLR2004
+
+    # combine the three masks using the binary "or" operation
+    # multiply the combined binary mask with the alpha channel
+    a = a * ((r_m == 1) | (g_m == 1) | (b_m == 1))
+
+    # stack the img back together
+    image = Image.fromarray(np.dstack([r, g, b, a]), 'RGBA')
+    try:
+        if dest:
+            image.save(dest)
+            yield Path(dest)
+        else:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmp = Path(tmpdir) / "tmp.png"
+                image.save(tmp)
+                if not tmp.exists():
+                    msg = f"File not found {tmp}"
+                    raise FileNotFoundError(msg)
+                yield tmp
+    finally:
+        pass
