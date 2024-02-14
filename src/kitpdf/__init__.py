4d95bb0d99cdd226ab946fcfe1f87d714c81b0f1
root <root@example.com>
2024-02-14 23:03:26 +0100

    fix:

--- src/kitpdf/__init__.py
+++ src/kitpdf/__init__.py
@@ -201,8 +201,12 @@ def pdf_from_picture(file: Path | str, picture: Path | str, rm: bool = True) -> 
 def pdf_linearize(file: Path | str) -> None:
     """Linearize pdf (overwrites original)."""
     nodeps.which("qpdf", raises=True)
-    subprocess.run(["qpdf", "--linearize", "--warning-exit-0", "--no-original-object-ids",
-                    "--replace-input", "--no-warn", file])
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp = Path(tmpdir) / "tmp.pdf"
+        subprocess.run(["qpdf", "--linearize", "--warning-exit-0", "--no-original-object-ids",
+                        "--no-warn", file, tmp])
+        Path(tmp).replace(file)
 
 
 def pdf_reduce(
